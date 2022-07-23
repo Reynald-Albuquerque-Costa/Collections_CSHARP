46396ac112b21d662d117dee6fd39c74353291bd
Reynald Albuquerque <reynaldalbuquerquecosta@gmail.com>
2022-07-22 22:47:00 -0300

    Exercícios Collections

---
+++ Queeu/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Queeu
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Fila !!!");
+
+            Queue<Pessoa> pessoas = new();
+
+            pessoas.Enqueue(new Pessoa("Reynald"));
+            pessoas.Enqueue(new Pessoa("Reinaldo"));
+            pessoas.Enqueue(new Pessoa("Renan"));
+            pessoas.Enqueue(new Pessoa("Dulce"));
+            pessoas.Enqueue(new Pessoa("Karol"));
+
+            Console.WriteLine($"O primeiro da fila é: {pessoas.Dequeue()}");
+
+            pessoas.Peek();
+
+            Console.WriteLine($"O número de pessoa na fila é : {pessoas.Count()}");
+
+            foreach (Pessoa pessoa in pessoas)
+            {
+                Console.WriteLine(pessoa.ToString());
+            }
+
+            }
+
+        public class Pessoa
+        {
+            public string Nome { get; set; }
+
+            public Pessoa(string nome)
+            {
+                Nome = nome;
+            }
+
+            public override string ToString()
+            {
+                return $"Nome: {Nome}";
+            }
+
+
+        }
+    }
+}
---
+++ list/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace list
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Lista de Animais!");
+
+            List<string> lista = new List<string>();
+
+            lista.Add("Leao");
+            lista.Add("Girafa");
+            lista.Add("Elefante");
+            lista.Add("Macaco");
+
+            lista.Insert(1, "Coruja");
+
+            lista.RemoveAt(4);
+
+            foreach (string nome in lista)
+            {
+                Console.WriteLine($"Aqui etá o nome {nome}");
+            }
+
+
+
+        }
+    }
+}
